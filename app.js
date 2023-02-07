e7b8ba87f6bb08d2862b1a774f0b1db82d582451
Samuel Alvarado <sam_zoom@hotmail.com>
2023-02-07 20:46:24 -0300

    Ejercicio3

---
+++ app.js
@@ -0,0 +1,65 @@
+/*
+let nombre = prompt("Ingrese su nombre")
+alert("Bienvenido " + nombre)
+
+
+let numero1 = parseInt(prompt("Ingrese el primer numero"))
+let numero2 = parseInt(prompt("Ingrese el segundo numero"))
+
+alert(numero1 + numero2)
+
+
+let numero1 = parseInt(prompt("Ingrese el primer numero"))
+let numero2 = parseInt(prompt("Ingrese el segundo numero"))
+
+if(numero1>numero2){
+    alert(numero1)
+}
+if(numero1<numero2){
+    alert(numero2)
+}
+
+
+let numero1 = parseInt(prompt("Ingrese el primer numero"))
+let numero2 = parseInt(prompt("Ingrese el segundo numero"))
+let numero3 = parseInt(prompt("Ingrese el tercer numero"))
+
+if(numero1 === numero2 && numero1 === numero3){
+    alert("Los numeros son iguales")
+}
+else{
+    alert("Los numeros no son iguales")
+}
+
+
+let numero1 = parseInt(prompt("Ingrese el primer numero"))
+let numero2 = parseInt(prompt("Ingrese el segundo numero"))
+let numero3 = parseInt(prompt("Ingrese el tercer numero"))
+
+???????????????????
+
+
+let peso = parseInt(prompt("Ingrese su peso en Kg"))
+let altura = parseInt(prompt("Ingrese su altura en metros"))
+
+imc = parseInt(peso / (altura^2))
+console.log(imc)
+
+if(imc<18.5){
+    alert("Bajo de peso")
+}
+if(imc>18.5 && imc<=24.9){
+    alert("Normal")
+}
+if(imc>=25 && imc<=29.9){
+    alert("Sobrepeso")
+}
+if(imc>30){
+    alert("Obesidad")
+}
+*/
+
+
+
+
+
